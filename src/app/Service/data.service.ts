6e5b0faa50487433cbf16e31e8e5eef36d2d57f9
Ashish Ranjan <codewithaashu1@gmail.com>
2023-06-26 21:27:45 +0530

    change baseURI with render server

--- src/app/Service/data.service.ts
+++ src/app/Service/data.service.ts
@@ -5,7 +5,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class DataService {
-  baseURI = 'http://localhost:9000';
+  // baseURI = 'http://localhost:9000';
+  baseURI = 'https://moviesmania-server.onrender.com';
   filterCriteria: any;
   trendingCriteria: Number = 7.2;
   latestCriteria: Number = 12;
